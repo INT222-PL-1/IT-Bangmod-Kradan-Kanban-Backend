d2300ceb5aa15d88d331fba5f79338e9cfba2dab
Tinnapop13 <tinnapoptienbang@gmail.com>
2024-04-24 16:54:31 +0700

    Fixed CrossOrigin

--- src/main/java/sit/int221/itbkkbackend/controllers/TaskController.java
+++ src/main/java/sit/int221/itbkkbackend/controllers/TaskController.java
@@ -8,7 +8,12 @@ import sit.int221.itbkkbackend.dtos.TaskDTO;
 import sit.int221.itbkkbackend.services.TaskService;
 
 import java.util.List;
-@CrossOrigin
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "http://ip23pl1.sit.kmutt.ac.th:5173",
+        "http://ip23pl1.sit.kmutt.ac.th:3000",
+})
 @RestController
 @RequestMapping("/v1/tasks")
 public class TaskController {
