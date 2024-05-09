e9161d85d7d87bcc8871ef9e4dadf0d289591414
Tinnapop13 <tinnapoptienbang@gmail.com>
2024-05-09 17:14:19 +0700

    add mapper config

--- src/main/java/sit/int221/itbkkbackend/ApplicationConfig.java
+++ src/main/java/sit/int221/itbkkbackend/ApplicationConfig.java
@@ -2,6 +2,7 @@ package sit.int221.itbkkbackend;
 
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sit.int221.itbkkbackend.utils.ListMapper;
@@ -10,7 +11,9 @@ import sit.int221.itbkkbackend.utils.ListMapper;
 public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
     @Bean
     public ListMapper listMapper() {return  new ListMapper(); }
