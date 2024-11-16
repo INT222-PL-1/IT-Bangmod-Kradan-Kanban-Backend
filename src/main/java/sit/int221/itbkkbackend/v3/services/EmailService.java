6043c085e9eee75ea21669d33958d96d8f36dc72
6MA-606 <zyxma.6666@hotmail.com>
2024-11-17 02:26:42 +0700

    Update: email styling

--- src/main/java/sit/int221/itbkkbackend/v3/services/EmailService.java
+++ src/main/java/sit/int221/itbkkbackend/v3/services/EmailService.java
@@ -1,8 +1,21 @@
 package sit.int221.itbkkbackend.v3.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import sit.int221.itbkkbackend.auth.CustomUserDetails;
+import sit.int221.itbkkbackend.auth.entities.Users;
 
 @Service
 public class EmailService {
@@ -25,5 +38,55 @@ public class EmailService {
         mailSender.send(message);
     }
 
+    public void sendHtmlEmail(String toEmail,
+                              String subject,
+                              String htmlBody
+    ) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("ITBKK-PL1 <itbkk.pl1@gmail.com>");
+        helper.setTo(toEmail);
+        helper.setText(htmlBody, true);
+        helper.setSubject(subject);
+        helper.setReplyTo("DO NOT REPLY <noreply@intproj23.sit.kmutt.ac.th>");
+        mailSender.send(message);
+    }
+
+    public void sendInvitationEmail(Users toUser, String accessRight, CustomUserDetails senderDetails, String boardId, String boardName) throws MessagingException, IOException {
+        String senderName = senderDetails.getName();
+        String toUsername = toUser.getName();
+        String toEmail = toUser.getEmail();
+        
+        String subject = String.format("%s has invited you to collaborate with %s access right on %s board", senderName, accessRight, boardName);
+        String link = String.format("https://intproj23.sit.kmutt.ac.th/pl1/board/%s/collab/invitations", boardId);
+        
+        String template = null;
+        String htmlBody = null;
+
+        try {
+            template = readResourceFile("templates/invitation_template.html");
+        } catch (IOException e) {
+            String body = "Hello,\n\n"
+                    + "You have been invited to collaborate on the board: " + boardName + ".\n\n"
+                    + "Please use the following link to either accept or decline the invitation:\n"
+                    + link + "\n\n"
+                    + "Thank you!\n\n";
+            sendSimpleEmail(toEmail, subject, body);
+            return;
+        }
+        htmlBody = template.replace("{{userName}}", toUsername)
+                        .replace("{{boardName}}", boardName)
+                        .replace("{{link}}", link)
+                        .replace("{{senderName}}", senderName)
+                        .replace("{{accessRight}}", accessRight);
+        sendHtmlEmail(toEmail, subject, htmlBody);
+    }
+
+    private String readResourceFile(String filePath) throws IOException {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
 }
 
